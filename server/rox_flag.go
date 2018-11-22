bd84d7049d6d232ea35fa62a899b860525be6620
Andrey Sokolov <falconandy@yandex.ru>
2018-11-22 11:03:18 +0400

    Simplification - currently RoxVariant === Variant, RoxFlag === Flag

--- server/rox_flag.go
+++ server/rox_flag.go
@@ -1,25 +1,12 @@
 package server
 
 import (
-	"github.com/rollout/rox-go/core/context"
 	"github.com/rollout/rox-go/core/entities"
 	"github.com/rollout/rox-go/core/model"
 )
 
-type RoxFlag interface {
-	model.Flag
-}
-
-type roxFlag struct {
-	model.Flag
-}
+type RoxFlag = model.Flag
 
 func NewRoxFlag(defaultValue bool) RoxFlag {
-	return &roxFlag{
-		Flag: entities.NewFlag(defaultValue),
-	}
-}
-
-func (f *roxFlag) InternalIsEnabled(ctx context.Context) (isEnabled bool, isDefault bool) {
-	return f.Flag.(model.InternalFlag).InternalIsEnabled(ctx)
+	return entities.NewFlag(defaultValue)
 }
--- server/rox_variant.go
+++ server/rox_variant.go
@@ -1,25 +1,12 @@
 package server
 
 import (
-	"github.com/rollout/rox-go/core/context"
 	"github.com/rollout/rox-go/core/entities"
 	"github.com/rollout/rox-go/core/model"
 )
 
-type RoxVariant interface {
-	model.Variant
-}
-
-type roxVariant struct {
-	model.Variant
-}
+type RoxVariant = model.Variant
 
 func NewRoxVariant(defaultValue string, options []string) RoxVariant {
-	return &roxVariant{
-		Variant: entities.NewVariant(defaultValue, options),
-	}
-}
-
-func (v *roxVariant) InternalGetValue(ctx context.Context) (returnValue string, isDefault bool) {
-	return v.Variant.(model.InternalVariant).InternalGetValue(ctx)
+	return entities.NewVariant(defaultValue, options)
 }
